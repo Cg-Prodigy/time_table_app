9c031bb71827f5ebe12f2bbd67e7f550a5b73dd7
Cg-Prodigy <brightmarshall2@gmail.com>
2022-05-14 20:30:46 +0300

    The applications home UI

--- app/main_ui.py
+++ app/main_ui.py
@@ -1,5 +1,28 @@
+
+from kivy.properties import BooleanProperty
+from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.screen import MDScreen
+from kivymd.uix.gridlayout import MDGridLayout
+from kivymd.uix.carousel import MDCarousel
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.label import MDLabel
 
-
-class EntryPoint(MDScreen):
+class Manager(ScreenManager):
     pass
+
+class HomeScreen(MDScreen):
+    pass
+
+class Task(MDGridLayout):
+    cols=2
+
+class TaskSlider(MDCarousel):
+    loop=BooleanProperty(True)
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        # for i in range(10):
+        #     text=MDLabel(text=f'Task at slide {i}',halign='center',adaptive_height=True)
+        #     box=MDBoxLayout(orientation='vertical')
+        #     box.add_widget(text)
+        #     self.add_widget(box)
+
--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from kivy.factory import Factory
 from kaki.app import App
 from kivymd.app import MDApp
 class HotReload(App,MDApp):
-    CLASSES={'EntryPoint':'app.main_ui'}
-    KV_FILES=['app/kivy_lang.kv']
+    CLASSES={'Manager':'app.main_ui'}
+    KV_FILES=['app/kivy_lang.kv','app/defaults.kv']
     AUTORELOADER_PATHS=[('.',{'recursive':True})]
     def build_app(self):
-        return Factory.EntryPoint()
+        return Factory.Manager()
 
 if __name__=='__main__':
     HotReload().run()
