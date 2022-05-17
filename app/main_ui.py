eda6e9d98335ef1ede367e18aabfe42f588feb14
Cg-Prodigy <brightmarshall2@gmail.com>
2022-05-17 12:38:23 +0300

    Added the create task screeen

--- app/main_ui.py
+++ app/main_ui.py
@@ -8,11 +8,15 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.label import MDLabel
 
 class Manager(ScreenManager):
-    pass
+    def switch_to_home_screen(self):
+        self.current='home screen'
+        self.transition.direction='right'
 
 class HomeScreen(MDScreen):
     pass
 
+class CreateTask(MDScreen):
+    pass
 class Task(MDGridLayout):
     cols=2
 
@@ -20,9 +24,10 @@ class TaskSlider(MDCarousel):
     loop=BooleanProperty(True)
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        # for i in range(10):
-        #     text=MDLabel(text=f'Task at slide {i}',halign='center',adaptive_height=True)
-        #     box=MDBoxLayout(orientation='vertical')
-        #     box.add_widget(text)
-        #     self.add_widget(box)
+        for i in range(10):
+            text=MDLabel(text=f'Task at slide {i}',halign='center',adaptive_height=True)
+            box=MDBoxLayout(orientation='vertical')
+            box.add_widget(text)
+            self.add_widget(box)
+
 
